7df6c960374e79c981d5ede00eea194d654ca512
Oseias Costa <oseiascosta@Mac-Pro-de-Oseias.local>
2023-06-19 19:53:07 -0300

    create project oral-brasil-xanxere

---
+++ app/GlobalStyle.tsx
@@ -0,0 +1,14 @@
+"use client";
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+* {
+  margin: 0;
+  padding: 0;
+  border: none;
+  list-style: none;
+  text-decoration: none;
+  box-sizing: border-box;
+  scroll-behavior: smooth;
+}
+`
---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+import styled from "styled-components"
+
+export default function Home() {
+  return (
+    <Container>
+
+    </Container>
+  )
+}
+
+const Container = styled.main`
+
+ 
+`
